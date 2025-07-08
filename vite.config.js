a310622e6cf1f5451763aa0196258f5161320d71
ahmad-dhuha-habibullah <122143363+ahmad-dhuha-habibullah@users.noreply.github.com>
2025-07-08 14:21:14 +0700

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  // Update this line to match your new repository name
+  base: '/SI-ROB-2/', 
   plugins: [vue()],
 })
